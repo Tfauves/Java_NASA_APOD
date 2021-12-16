1895c6e013eda3bfb00a0484e4bc982055325f1a
Travis J <tfauvelle@gmail.com>
2021-12-15 22:50:54 -0500

    refactored for testing.

--- src/main/java/com/example/demo/payload/api/response/Breed.java
+++ src/main/java/com/example/demo/payload/api/response/Breed.java
@@ -32,5 +32,49 @@ public class Breed {
         }
     }
 
+    private Source source;
+    private String name ;
+    private String breed_group;
+    private String life_span;
 
+    public Breed() {}
+
+    public Breed(Source source, String name, String breed_group, String life_span) {
+        this.source = source;
+        this.name = name;
+        this.breed_group = breed_group;
+        this.life_span = life_span;
+    }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed_group() {
+        return breed_group;
+    }
+
+    public void setBreed_group(String breed_group) {
+        this.breed_group = breed_group;
+    }
+
+    public String getLife_span() {
+        return life_span;
+    }
+
+    public void setLife_span(String life_span) {
+        this.life_span = life_span;
+    }
 }
